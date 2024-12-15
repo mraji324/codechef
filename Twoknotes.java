9e5c6d3ce75e88adcd3ca4c2c19554ee0a176444
Hindu <rajuhindur@gmail.com>
2024-12-15 12:03:10 +0530

    Add files via upload

---
+++ Twoknotes.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Twoknotes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+
+        int notesNeeded = (n * 2000) / 500;
+        System.out.println(+ notesNeeded);
+	}
+}
